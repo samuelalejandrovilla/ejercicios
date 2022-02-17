2ec3c43e3de4b51916a9f497df842e3ed87f7fac
samuelalejandrovilla <99101421+samuelalejandrovilla@users.noreply.github.com>
2022-02-16 21:20:19 -0600

    Update and rename kata1.py to mio.py

--- kata1.py
+++
@@ -1,7 +0,0 @@
-from datetime import date
-
-print("Today's date is: " + str(date.today()))
-parsec = 11
-lightyears = 3.26156 * parsec
-
-print(str(parsec) + " parsec, is " + str(lightyears) + " lightyears")
